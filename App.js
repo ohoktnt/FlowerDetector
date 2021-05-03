ce0b15e26199218d2802fa8cb35a98f886d22464
Tammy <tammy.n.tran@gmail.com>
2021-05-03 15:33:18 -0400

    added buttons, added safe area context for buttons to work

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import React, {Component} from 'react';
-import {View, Text, Image, StyleSheet}  from 'react-native';
+import {View, Text, Image, StyleSheet} from 'react-native';
 import {Button} from 'react-native-elements';
-import LinearGradient from 'react-native-linear-gradient'; 
+import LinearGradient from 'react-native-linear-gradient';
 
 export default class App extends Component {
   render() {
-    return(<LinearGradient colors={['#a8e063', '#56ab2f']} style={styles.linearGradient}></LinearGradient>);
+    return (
+      <LinearGradient
+        colors={['#a8e063', '#56ab2f']}
+        style={styles.linearGradient}>
+        {/* Title and subtitle */}
+        <View>
+          <Text>Find Flowers</Text>
+          <Text>Python Neural Network</Text>
+        </View>
+        {/* Output */}
+        <View>
+          <Text>Output</Text>
+        </View>
+        {/* Buttons */}
+        <View>
+          <Button
+            title="Camera Roll"
+            buttonStyle={styles.button}
+            containerStyle={{margin: 5}}
+            titleStyle={{fontSize: 20}}></Button>
+          <Button
+            title="Take a Photo"
+            buttonStyle={styles.button}
+            containerStyle={{margin: 5}}
+            titleStyle={{fontSize: 20}}></Button>
+        </View>
+      </LinearGradient>
+    );
   }
 }
 
@@ -13,4 +40,4 @@ const styles = StyleSheet.create({
   linearGradient: {
     flex: 1,
   },
-})
+});
